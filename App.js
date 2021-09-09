9240d341008a911eca5a11535fbf2913ada9cfc9
tush1308 <80100538+tush1308@users.noreply.github.com>
2021-09-09 13:40:42 +0530

    contacts app files added

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import MyContacts from './screens/MyContacts'
+import Profile from './screens/Profile'
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='MyContacts'>
+        <Stack.Screen name='MyContacts' component={MyContacts} />
+        <Stack.Screen name='Profile' component={Profile}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
